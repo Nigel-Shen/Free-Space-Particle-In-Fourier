800129674d159cc4e85413799671748f3b720c0d
Nigel Shen <1240234313@qq.com>
2023-09-01 23:10:36 -0400

    Update Dirichlet_PIF_cyclotron.py

--- Dirichlet_PIF_cyclotron.py
+++ Dirichlet_PIF_cyclotron.py
@@ -1,7 +1,7 @@
 """
 MIT License
 
-Copyright (c) Nigel Shen
+Copyright (c) 2023 Nigel Shen
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
